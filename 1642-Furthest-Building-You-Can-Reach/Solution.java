24d949afca1a8ece10b9a60f01dd5849168f0531
Geverton <gevertoninc@gmail.com>
2022-06-21 01:52:14 -0300

    Solve problem 1642

---
+++ 1642-Furthest-Building-You-Can-Reach/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < heights.length - 1; i++) {
+            int next = heights[i + 1];
+            int current = heights[i];
+            int distance = next - current;
+
+            if (distance > 0)
+                priorityQueue.add(distance);
+            if (priorityQueue.size() > ladders)
+                bricks -= priorityQueue.poll();
+            if (bricks < 0)
+                return i;
+        }
+
+        return heights.length - 1;
+    }
+}
